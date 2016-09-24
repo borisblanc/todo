f0a31aa8f00636147770267f027143d914f02504
borisblanc <bblanc>
2016-09-23 21:42:42 -0400

    first commit

---
+++ back/server.js
@@ -0,0 +1,50 @@
+var http = require("http");
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer(function(req, res) {
+	var filename;
+	var savefile = "./tasks.json";
+
+	switch(req.url) {
+		case "/app.js":
+			filename = 'app.js';
+			break;
+		case '/':
+			filename = 'index.html';
+			break;
+		case '/tasks':
+			if (req.method == "POST") {
+				var ws = fs.createWriteStream(savefile);
+				req.on('data', function (chunk) {
+					console.log("Received body data:");
+					console.log(chunk.toString());
+				});
+				req.pipe(ws);
+				req.on('end', function () {
+					res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+					res.end();
+				});
+				return;
+			}
+			if (req.method == "GET") {
+				var savedtasks = fs.createReadStream(savefile);
+				savedtasks.pipe(res);
+				return;
+			}
+			break;
+		default:
+			res.writeHead(404, "file does not exists");
+			res.end();
+			return;
+	}
+	var fpath = path.join(__dirname, '../front', filename);
+	var rs = fs.createReadStream(fpath);
+	rs.pipe(res);
+});
+
+server.listen(8080, "localhost", function(){
+	console.log("server is listening...");
+});
+
+
---
+++ front/task.js
@@ -0,0 +1,140 @@
+
+var template;
+
+var tasksCollection = [];
+tasksCollection.createTask = function(){
+	var task = {};
+	task.id = this.length;
+	task.name = "";
+	task.description = "";
+	task.priority = 0;
+	task.isDone = 0;
+	this.push(task);
+	return task;
+};
+
+function showTasks() {
+	console.log(tasksCollection);
+}
+
+function popTasks(tasks) {
+	var divtasks = document.querySelector('.tasks');
+
+	for (var i = 0; i < tasks.length; i++) {
+		var domTaskRow = template.cloneNode(true);
+		var task = tasks[i];
+		domTaskRow.getElementsByClassName('name')[0].value = task.name;
+		domTaskRow.getElementsByClassName('description')[0].value = task.description;
+		domTaskRow.getElementsByClassName('priority')[0].value = task.priority;
+		domTaskRow.getElementsByClassName('is-done')[0].value = task.isDone;
+		divtasks.appendChild(domTaskRow);
+		addTaskRowEventListeners(domTaskRow,task);
+		tasksCollection[i] = task;
+	}
+}
+
+
+
+function addTaskRowEventListeners(domTaskRow, task) {
+	domTaskRow.addEventListener('keyup', function(event){
+		var target = event.target;
+
+		if(target.classList.contains('name')){
+			task.name = target.value;
+		}
+		if(target.classList.contains('description')){
+			task.description = target.value;
+		}
+		if(target.classList.contains('priority')){
+			task.priority = target.value;
+		}
+	});
+
+	domTaskRow.addEventListener('change', function(event) {
+		var target = event.target;
+		if(target.classList.contains('is-done')){
+			task.isDone = target.checked;
+		}
+	});
+}
+
+document.addEventListener("DOMContentLoaded", function(event){
+	var buttoncreate = document.querySelector('.create-button');
+	var buttonsave = document.querySelector('.save-button');
+	var tasks = document.querySelector('.tasks');
+
+	//load template
+	var html = document.querySelector('#task-template').innerHTML;
+	template = document.createElement('div');
+	template.innerHTML = html;
+
+
+	// get data
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', '/tasks', true);
+	xhr.onload = function () {
+		if (xhr.readyState === 4) {
+			if (xhr.status === 200) {
+				console.log(xhr.responseText);
+				var tasks = JSON.parse(xhr.responseText);
+				popTasks(tasks);
+			} else {
+				console.error(xhr.statusText);
+			}
+		}
+	};
+	xhr.onerror = function () {
+		console.error(xhr.statusText);
+	};
+	xhr.send(null);
+
+
+	buttoncreate.addEventListener('click', function(){
+		var domTaskRow = template.cloneNode(true);
+		var task = tasksCollection.createTask();
+		addTaskRowEventListeners(domTaskRow, task);
+		tasks.appendChild(domTaskRow);
+	});
+
+
+	buttonsave.addEventListener('click', function () {
+		if (tasksCollection.length > 0) {
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', '/tasks', true);
+			xhr.setRequestHeader("Content-Type", "application/json");
+
+			xhr.onload = function () {
+				if (xhr.readyState === 4) {
+					if (xhr.status === 200) {
+						console.log(xhr.responseText);
+					} else {
+						console.error(xhr.statusText);
+					}
+				}
+			};
+			xhr.onerror = function () {
+				console.error(xhr.statusText);
+			};
+
+			xhr.send(JSON.stringify(tasksCollection));
+		}
+	});
+
+/*	tasks
+	urls - crud operations on tasks
+
+	GET /tasks
+	GET /tasks/id  id of a task to retrieve
+	PUT /tasks/id
+	DELETE /tasks/id
+
+
+
+	POST - creating new tasks
+	GET - retrieving existings tasks
+	PUT - updating exsitings tasks
+	DELETE - deleteing tasks*/
+
+
+
+});
